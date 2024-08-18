ec74f820a7e62b6f3fdfc88b1752235d243caa1f
Ishan Phadte <ishanphadte@gmail.com>
2024-08-17 21:38:55 -0400

    adding files

---
+++ KakfaSCS/src/main/java/com/ishan/phadte/ishan_kafka_demo/IshanKafkaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ishan.phadte.ishan_kafka_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IshanKafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IshanKafkaDemoApplication.class, args);
+	}
+
+}
---
+++ KakfaSCS/src/test/java/com/ishan/phadte/ishan_kafka_demo/IshanKafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishan.phadte.ishan_kafka_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IshanKafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
